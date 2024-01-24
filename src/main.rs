fb1bcc1b9166b7e3cb5146687220dafbd536c945
Dan Liebgold <dan.liebgold@gmail.com>
2024-01-24 03:00:41 -0800

    proper animation

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ struct SpriteInfo {
     sheet: String,
     columns: usize,
     rows: usize,
-    frames: Vec<i32>,
+    frames: Vec<usize>,
 }
 
 #[derive(Resource)]
@@ -39,30 +39,37 @@ fn main() {
 }
 
 #[derive(Component)]
-struct AnimationIndices {
-    first: usize,
-    last: usize,
-}
+struct AnimationFrame(i32);
 
 #[derive(Component, Deref, DerefMut)]
 struct AnimationTimer(Timer);
 
 fn animate_sprite(
     time: Res<Time>,
+    sprite_info: Res<SpriteInfoHandle>,
+    sprite_infos: Res<Assets<SpriteInfo>>,
     mut query: Query<(
-        &AnimationIndices,
+        &mut AnimationFrame,
         &mut AnimationTimer,
         &mut TextureAtlasSprite,
     )>,
 ) {
-    for (indices, mut timer, mut sprite) in &mut query {
-        timer.tick(time.delta());
-        if timer.just_finished() {
-            sprite.index = if sprite.index == indices.last {
-                indices.first
-            } else {
-                sprite.index + 1
-            };
+    if let Some(info) = sprite_infos.get(sprite_info.0.id()) {
+        for (mut frame, mut timer, mut sprite) in &mut query {
+            timer.tick(time.delta());
+            if timer.just_finished() {
+                frame.0 += 1;
+                if frame.0 == info.frames.len() as i32 {
+                    frame.0 = 0
+                }
+                sprite.index = info.frames[frame.0 as usize]
+                //if frame ==
+                // sprite.index = if sprite.index == indices.last {
+                //     indices.first
+                // } else {
+                //     sprite.index + 1
+                // };
+            }
         }
     }
 }
@@ -80,27 +87,28 @@ fn spawn_sprites(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     sprite_info: Res<SpriteInfoHandle>,
-    mut sprite_infos: ResMut<Assets<SpriteInfo>>,
+    sprite_infos: Res<Assets<SpriteInfo>>,
     mut texture_atlases: ResMut<Assets<TextureAtlas>>,
     mut state: ResMut<NextState<AppState>>,
 ) {
-    if let Some(info) = sprite_infos.remove(sprite_info.0.id()) {
+    if let Some(info) = sprite_infos.get(sprite_info.0.id()) {
         let texture_handle = asset_server.load(info.sheet.clone());
         //let texture_handle = asset_server.load("gabe-idle-run.png");
         let texture_atlas =
             TextureAtlas::from_grid(texture_handle, Vec2::new(24.0, 24.0), info.columns, info.rows, None, None);
         let texture_atlas_handle = texture_atlases.add(texture_atlas);
         // Use only the subset of sprites in the sheet that make up the run animation
-        let animation_indices = AnimationIndices { first: 23, last: 28 };
+        //let animation_indices = AnimationIndices { first: 23, last: 28 };
+        let animation_frame = AnimationFrame(0);
         commands.spawn(Camera2dBundle::default());
         commands.spawn((
             SpriteSheetBundle {
                 texture_atlas: texture_atlas_handle,
-                sprite: TextureAtlasSprite::new(animation_indices.first),
+                sprite: TextureAtlasSprite::new(info.frames[0]),
                 transform: Transform::from_scale(Vec3::splat(6.0)),
                 ..default()
             },
-            animation_indices,
+            animation_frame,
             AnimationTimer(Timer::from_seconds(0.2, TimerMode::Repeating)),
         ));
         state.set(AppState::Level);
