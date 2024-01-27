f71e57ef383ec458537d398986ad63cccc9eed4b
Dan Liebgold <dliebgold@wonderstorm.net>
2024-01-26 23:24:56 -0800

    setting up tile layers

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@
 //! into a texture atlas, and changing the displayed image periodically.
 
 use bevy::prelude::*;
-use bevy_ecs_tilemap::prelude::*;
 use bevy_common_assets::ron::RonAssetPlugin;
+use bevy_ecs_tilemap::prelude::*;
 use bevy_pancam::{PanCam, PanCamPlugin};
 
 mod helpers;
@@ -15,8 +15,7 @@ enum AppState {
     Level,
 }
 
-
-#[derive(serde::Deserialize,bevy::asset::Asset,bevy::reflect::TypePath)]
+#[derive(serde::Deserialize, bevy::asset::Asset, bevy::reflect::TypePath)]
 struct SpriteInfo {
     sheet: String,
     columns: usize,
@@ -29,11 +28,13 @@ struct SpriteInfoHandle(Handle<SpriteInfo>);
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest())) // prevents blurry sprites
-        .add_plugins(RonAssetPlugin::<SpriteInfo>::new(&["sprite-info.ron"]))
-        .add_plugins(PanCamPlugin::default())
-        .add_plugins(TilemapPlugin)
-        .add_plugins(helpers::tiled::TiledMapPlugin)
+        .add_plugins((
+            DefaultPlugins.set(ImagePlugin::default_nearest()), // prevents blurry sprites
+            RonAssetPlugin::<SpriteInfo>::new(&["sprite-info.ron"]),
+            PanCamPlugin::default(),
+            TilemapPlugin,
+            helpers::tiled::TiledMapPlugin,
+        ))
         .add_state::<AppState>()
         .add_systems(Startup, setup.run_if(in_state(AppState::Loading)))
         .add_systems(Update, spawn_sprites.run_if(in_state(AppState::Loading)))
@@ -71,25 +72,22 @@ fn animate_sprite(
     }
 }
 
-fn setup(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let sprite_info_handle = SpriteInfoHandle(asset_server.load("main.sprite-info.ron"));
     commands.insert_resource(sprite_info_handle);
 
-    let map_handle: Handle<helpers::tiled::TiledMap> = asset_server.load("maps/TMX/oryx_16-bit_fantasy_test.tmx");
+    let map_handle: Handle<helpers::tiled::TiledMap> =
+        asset_server.load("maps/TMX/oryx_16-bit_fantasy_test.tmx");
 
     commands.spawn(helpers::tiled::TiledMapBundle {
         tiled_map: map_handle,
+        // transform: Transform::from_scale(Vec3::splat(1.0))
+        //     .with_translation(Vec3::new(0.0, 0.0, 0.1)),
         ..Default::default()
     });
 
-    commands.spawn((
-        Camera2dBundle::default(),
-        PanCam::default()));
+    commands.spawn((Camera2dBundle::default(), PanCam::default()));
 }
-
 
 fn spawn_sprites(
     mut commands: Commands,
@@ -102,8 +100,14 @@ fn spawn_sprites(
     if let Some(info) = sprite_infos.get(sprite_info.0.id()) {
         let texture_handle = asset_server.load(info.sheet.clone());
         //let texture_handle = asset_server.load("gabe-idle-run.png");
-        let texture_atlas =
-            TextureAtlas::from_grid(texture_handle, Vec2::new(24.0, 24.0), info.columns, info.rows, None, None);
+        let texture_atlas = TextureAtlas::from_grid(
+            texture_handle,
+            Vec2::new(24.0, 24.0),
+            info.columns,
+            info.rows,
+            None,
+            None,
+        );
         let texture_atlas_handle = texture_atlases.add(texture_atlas);
         // Use only the subset of sprites in the sheet that make up the run animation
         //let animation_indices = AnimationIndices { first: 23, last: 28 };
@@ -112,7 +116,8 @@ fn spawn_sprites(
             SpriteSheetBundle {
                 texture_atlas: texture_atlas_handle,
                 sprite: TextureAtlasSprite::new(info.frames[0]),
-                transform: Transform::from_scale(Vec3::splat(6.0)),
+                transform: Transform::from_scale(Vec3::splat(6.0))
+                    .with_translation(Vec3::new(48.0, 48.0, 1.0)),
                 ..default()
             },
             animation_frame,
