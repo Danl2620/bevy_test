c377d14484fd626864835c088fe23b13908f2ad9
Dan Liebgold <dan.liebgold@gmail.com>
2024-01-29 18:45:49 -0800

    spawning tile map; attempting object spawn position

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@
 use bevy::prelude::*;
 use bevy_asset_loader::asset_collection::AssetCollection;
 use bevy_asset_loader::prelude::*;
-//use bevy_common_assets::ron::RonAssetPlugin;
 use bevy_ecs_tilemap::prelude::*;
 use bevy_pancam::{PanCam, PanCamPlugin};
 
@@ -17,38 +16,18 @@ enum AppState {
     Level,
 }
 
-// #[derive(serde::Deserialize, bevy::reflect::TypePath)]
-// struct SpriteInfo {
-//     sheet: String,
-//     columns: usize,
-//     rows: usize,
-//     frames: Vec<usize>,
-// }
-
-// #[derive(serde::Deserialize, bevy::asset::Asset, bevy::reflect::TypePath)]
-// struct GameInfo {
-//     sprite_info: SpriteInfo,
-//     map_name: String,
-// }
-
 #[derive(AssetCollection, Resource)]
 struct GameInfoAlt {
     #[asset(key = "atlas.creatures")]
     creature_atlas: Handle<TextureAtlas>,
-    // #[asset(key = "pairs.creatures")]
-    // creatures: Handle<(i16, i16)>,
     #[asset(key = "map.main")]
-    map_name: Handle<helpers::tiled::TiledMap>,
+    tile_map: Handle<helpers::tiled::TiledMap>,
 }
-
-// #[derive(Resource)]
-// struct GameInfoHandle(Handle<GameInfo>);
 
 fn main() {
     App::new()
         .add_plugins((
             DefaultPlugins.set(ImagePlugin::default_nearest()), // prevents blurry sprites
-            //RonAssetPlugin::<GameInfo>::new(&["game-info.ron"]),
             PanCamPlugin::default(),
             TilemapPlugin,
             helpers::tiled::TiledMapPlugin,
@@ -60,9 +39,7 @@ fn main() {
                 .with_dynamic_assets_file::<StandardDynamicAssetCollection>("main.assets.ron")
                 .load_collection::<GameInfoAlt>(),
         )
-        //.add_systems(Startup, setup.run_if(in_state(AppState::Loading)))
-        //.add_systems(Update, spawn_sprites.run_if(in_state(AppState::Loading)))
-        .add_systems(OnEnter(AppState::Level), spawn_sprites)
+        .add_systems(OnEnter(AppState::Level), spawn_level)
         .add_systems(Update, animate_sprite.run_if(in_state(AppState::Level)))
         .run();
 }
@@ -75,7 +52,6 @@ struct AnimationTimer(Timer);
 
 fn animate_sprite(
     time: Res<Time>,
-    game_info: Res<GameInfoAlt>,
     mut query: Query<(
         &mut AnimationFrame,
         &mut AnimationTimer,
@@ -94,61 +70,55 @@ fn animate_sprite(
     }
 }
 
-// fn setup(
-//     mut commands: Commands,
-//     asset_server: Res<AssetServer>,
-//     game_infos: Res<Assets<GameInfo>>,
-// ) {
-//     let game_info_handle = asset_server.load("main.game-info.ron");
-//     commands.insert_resource(GameInfoHandle(game_info_handle.clone()));
-
-//     if let Some(info) = game_infos.get(&game_info_handle) {
-//         let path = info.map_name.clone();
-//         info!("{:?}", path);
-//         let map_handle: Handle<helpers::tiled::TiledMap> =
-//             // asset_server.load("maps/TMX/oryx_16-bit_fantasy_test.tmx");
-//             asset_server.load(path);
-
-//         commands.spawn(helpers::tiled::TiledMapBundle {
-//             tiled_map: map_handle,
-//             // transform: Transform::from_scale(Vec3::splat(1.0))
-//             //     .with_translation(Vec3::new(0.0, 0.0, 0.1)),
-//             ..Default::default()
-//         });
-//     }
-// }
-
-fn spawn_sprites(
+fn spawn_level(
     mut commands: Commands,
     game_info: Res<GameInfoAlt>,
+    tile_maps: Res<Assets<helpers::tiled::TiledMap>>,
     mut state: ResMut<NextState<AppState>>,
 ) {
     info!("spawn_sprites");
-    //let texture_handle = asset_server.load(info.sprite_info.sheet.clone());
-    //let texture_handle = asset_server.load("gabe-idle-run.png");
-    // let texture_atlas = TextureAtlas::from_grid(
-    //     texture_handle,
-    //     Vec2::new(24.0, 24.0),
-    //     info.sprite_info.columns,
-    //     info.sprite_info.rows,
-    //     None,
-    //     None,
-    // );
-    // let texture_atlas_handle = texture_atlases.add(texture_atlas);
-    // Use only the subset of sprites in the sheet that make up the run animation
-    //let animation_indices = AnimationIndices { first: 23, last: 28 };
-    let animation_frame = AnimationFrame(22);
-    commands.spawn((
-        SpriteSheetBundle {
-            texture_atlas: game_info.creature_atlas.clone(),
-            sprite: TextureAtlasSprite::new(22),
-            transform: Transform::from_scale(Vec3::splat(6.0))
-                .with_translation(Vec3::new(48.0, 48.0, 2.0)),
-            ..default()
-        },
-        animation_frame,
-        AnimationTimer(Timer::from_seconds(0.2, TimerMode::Repeating)),
-    ));
+
+    commands.spawn(helpers::tiled::TiledMapBundle {
+        tiled_map: game_info.tile_map.clone(),
+        transform: Transform::from_scale(Vec3::splat(1.0))
+            .with_translation(Vec3::new(0.0, 0.0, 0.1)),
+        ..Default::default()
+    });
+
+    // spawn characters
+    if let Some(map) = tile_maps.get(&game_info.tile_map) {
+        info!("spawn objects");
+        let tile_layers = map
+            .map
+            .layers()
+            .filter_map(|layer| match layer.layer_type() {
+                tiled::LayerType::Objects(layer) => Some(layer),
+                _ => None,
+            });
+
+        for layer in tile_layers {
+            //my_renderer.render(layer);
+            for object in layer.objects() {
+                if object.visible && object.user_type.eq_ignore_ascii_case("spawn") {
+                    info!("spawning {}\n", object.name);
+
+                    let animation_frame = AnimationFrame(0);
+                    commands.spawn((
+                        SpriteSheetBundle {
+                            texture_atlas: game_info.creature_atlas.clone(),
+                            sprite: TextureAtlasSprite::new(22),
+                            transform: Transform::from_translation(Vec3::new(
+                                object.x, object.y, 2.0,
+                            )),
+                            ..default()
+                        },
+                        animation_frame,
+                        AnimationTimer(Timer::from_seconds(0.2, TimerMode::Repeating)),
+                    ));
+                }
+            }
+        }
+    }
 
     commands.spawn((Camera2dBundle::default(), PanCam::default()));
     state.set(AppState::Level);
